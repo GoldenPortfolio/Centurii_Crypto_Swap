3f4daf776c6310dd61f5aa669f8a4d2e763eca81
GoldenPortfolio <goldpalmvista@gmail.com>
2022-09-18 17:46:15 -0400

    first commit

---
+++ packages/react-app/src/App.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { useEthers } from "@usedapp/core";
+
+import styles from "./styles";
+import { usePools } from "./hooks";
+import { Centurii } from "./assets/";
+import { Exchange, Loader, WalletButton } from "./components";
+
+const App = () => {
+  const { account } = useEthers();
+  const [poolsLoading, pools] = usePools();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.innerContainer}>
+        <header className={styles.header}>
+          <img
+            src={Centurii}
+            alt="Centurii-Logo"
+            className="w-15 h-14 object-contain"
+          />
+          <WalletButton />
+        </header>
+
+        <div className={styles.exchangeContainer}>
+          <h1 className={styles.headTitle}>Centurii Insurance</h1>
+          <p className={styles.subTitle}>Exchange tokens and insure your crypto in seconds</p>
+          <div className={styles.exchangeBoxWrapper}>
+            <div className={styles.exchangeBox}>
+              <div className="pink_gradient" />
+              <div className={styles.exchange}>
+                {account ? (
+                  poolsLoading ? (
+                    <Loader title="Loading pools, please wait!" />
+                  ) : (
+                    <Exchange pools={pools} />
+                  )
+                ) : (
+                  <Loader title="Please connect your wallet" />
+                )}
+              </div>
+              <div className="blue_gradient" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
